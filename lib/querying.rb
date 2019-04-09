9a9fa51601787980f9f85caa3878198f99b244ee
Daniel Schneider <daschne8@gmail.com>
2019-04-09 21:18:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  ;"
+  GROUP BY series.title;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
