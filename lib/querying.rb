7d077705f9788d541478a9c11fd323fa25ef32a6
Daniel Schneider <daschne8@gmail.com>
2019-04-09 21:19:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,9 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY series.title;"
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species)
+  WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
