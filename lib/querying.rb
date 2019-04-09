035341cb4763ae2816b59bd68c7cb488353c2d12
Daniel Schneider <daschne8@gmail.com>
2019-04-09 21:19:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON series.id = characters.series_id
   GROUP BY series.title
-  
+
   HAVING characters.species = 'human';"
 end
 
